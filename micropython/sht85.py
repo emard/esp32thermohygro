0bdaa4973fb14d5e96c4620743811b0ed70ec1ac
Emard <vordah@gmail.com>
2026-06-15 23:30:06 +0200

    sht85 support

---
+++ micropython/sht85.py
@@ -0,0 +1,38 @@
+from machine import Pin,SoftI2C
+from time import sleep_ms
+
+# SHT85 default I2C address
+SHT85_ADDR = const(0x44)
+
+# Measurement command: High repeatability, clock stretching disabled (0x2400)
+MEASURE_CMD = b'\x24\x00'
+
+class SHT85:
+  def __init__(self, sck_pin:int, data_pin:int):
+    self.i2c=SoftI2C(scl=Pin(sck_pin), sda=Pin(data_pin), freq=100000)
+
+  def read_temp_humidity(self)->Tuple(float,float):
+    # Send measurement command
+    try:
+      self.i2c.writeto(SHT85_ADDR, MEASURE_CMD)
+    except: # ENODEV
+      return -99, 0
+    
+    # Wait for the measurement to complete (max ~15.5ms)
+    sleep_ms(20)
+    
+    # Read 6 bytes: Temp MSB, Temp LSB, Temp CRC, Hum MSB, Hum LSB, Hum CRC
+    data = self.i2c.readfrom(SHT85_ADDR, 6)
+    
+    if len(data) == 6:
+        # Convert Temperature Data
+        temp_raw = (data[0] << 8) | data[1]
+        temperature = -45.0 + (175.0 * temp_raw / 65535.0)
+        
+        # Convert Humidity Data
+        hum_raw = (data[3] << 8) | data[4]
+        humidity = -6.0 + (125.0 * hum_raw / 65535.0)
+        
+        return temperature, humidity
+    else:
+        return -99, 0
--- micropython/thermohygro.py
+++ micropython/thermohygro.py
@@ -1,12 +1,15 @@
 from sht75 import SHT75
+from sht85 import SHT85
 import ssd1306txt
 
 sensor1=SHT75(sck_pin=3, data_pin=4)
-sensor2=SHT75(sck_pin=1, data_pin=2)
+#sensor1=SHT85(sck_pin=3, data_pin=4)
+#sensor2=SHT75(sck_pin=1, data_pin=2)
+sensor2=SHT85(sck_pin=1, data_pin=2)
 
 while True:
   t1,rh1=sensor1.read_temp_humidity()
   ssd1306txt.thdisp(1,t1,rh1)
   t2,rh2=sensor2.read_temp_humidity()
   ssd1306txt.thdisp(2,t2,rh2)
-  print("T1=%5.2f C RH1=%5.2f %% T1=%5.2f C RH1=%5.2f %% " % (t1,rh1,t2,rh2))
+  print("T1=%5.2f C RH1=%5.2f %% T2=%5.2f C RH2=%5.2f %% " % (t1,rh1,t2,rh2))
