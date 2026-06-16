a3ee3a4cf728938e5cf81d5f8dd98f23cf5090f4
Emard <vordah@gmail.com>
2026-06-16 18:51:35 +0200

    prints autoedetected sensor model SHT75/SHT85

--- micropython/thermohygro.py
+++ micropython/thermohygro.py
@@ -9,19 +9,29 @@ sensor2_sda_pin=const(2)
 
 t1=-99.9
 rh1=-99.9
+model1=""
 t2=-99.9
 rh2=-99.9
+model2=""
 while True:
   if t1<-99:
+    model1="SHT85"
     sensor1=SHT85(sck_pin=sensor1_scl_pin, data_pin=sensor1_sda_pin)
     if sensor1.detect()==False:
+      model1="SHT75"
       sensor1=SHT75(sck_pin=sensor1_scl_pin, data_pin=sensor1_sda_pin)
   t1,rh1=sensor1.read_temp_humidity()    
-  ssd1306txt.thdisp(1,t1,rh1)
+  if t1<-99:
+    model1=""
+  ssd1306txt.thdisp(1,model1,t1,rh1)
   if t2<-99:
+    model2="SHT85"
     sensor2=SHT85(sck_pin=sensor2_scl_pin, data_pin=sensor2_sda_pin)
     if sensor2.detect()==False:
+      model2="SHT75"
       sensor2=SHT75(sck_pin=sensor2_scl_pin, data_pin=sensor2_sda_pin)
   t2,rh2=sensor2.read_temp_humidity()
-  ssd1306txt.thdisp(2,t2,rh2)
+  if t2<-99:
+    model2=""
+  ssd1306txt.thdisp(2,model2,t2,rh2)
   print("T1=%5.2f C RH1=%5.2f %% T2=%5.2f C RH2=%5.2f %% " % (t1,rh1,t2,rh2))
