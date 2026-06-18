448f39873fe1b0205fc9c068947deb48fb66505a
davor <davor@brix16.lan>
2026-06-18 10:18:02 +0200

    crc table in separate module check crc for sht85

--- micropython/sht85.py
+++ micropython/sht85.py
@@ -1,5 +1,6 @@
 from machine import Pin,SoftI2C
 from time import sleep_ms
+from shtcrctab import crctable
 
 # SHT85 default I2C address
 SHT85_ADDR = const(0x44)
@@ -16,28 +17,40 @@ class SHT85:
       return True
     return False
 
+  def crc_calc(self, msb:int, lsb:int)->int:
+    crc = 0xFF
+    crc = crctable[crc ^ msb]
+    crc = crctable[crc ^ lsb]
+    return crc
+
   def read_temp_humidity(self)->Tuple(float,float):
     # Send measurement command
     try:
       self.i2c.writeto(SHT85_ADDR, MEASURE_CMD)
     except: # ENODEV
       return -99.9, -99.9
-    
+
     # Wait for the measurement to complete (max ~15.5ms)
     sleep_ms(20)
-    
+
     # Read 6 bytes: Temp MSB, Temp LSB, Temp CRC, Hum MSB, Hum LSB, Hum CRC
     data = self.i2c.readfrom(SHT85_ADDR, 6)
-    
+
     if len(data) == 6:
         # Convert Temperature Data
-        temp_raw = (data[0] << 8) | data[1]
-        temperature = 175 * temp_raw / 65535 - 45
-        
+        if self.crc_calc(data[0],data[1]) == data[2]:
+          temp_raw = (data[0] << 8) | data[1]
+          temperature = 175 * temp_raw / 65535 - 45
+        else:
+          temperature = -99.9
+
         # Convert Humidity Data
-        hum_raw = (data[3] << 8) | data[4]
-        humidity = 100 * hum_raw / 65535
-        
+        if self.crc_calc(data[3],data[4]) == data[5]:
+          hum_raw = (data[3] << 8) | data[4]
+          humidity = 100 * hum_raw / 65535
+        else:
+          humidity = -99.9
+
         return temperature, humidity
     else:
         return -99.9, -99.9
