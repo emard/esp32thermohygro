6afad66a853c2f50e6f81363263b5b899013efbc
Emard <vordah@gmail.com>
2026-09-21 22:52:28 +0200

    name/pass

--- micropython/tools/wificonnect.py
+++ micropython/tools/wificonnect.py
@@ -1,8 +1,8 @@
 # simple connect as client to AP
 # for small web server like microdot
 import network
-NAME="ra"
-PASS="GigabyteBrix"
+NAME="name"
+PASS="password"
 wifi=network.WLAN(network.STA_IF)
 wifi.active(True)
 #wifi.config(txpower=13)
